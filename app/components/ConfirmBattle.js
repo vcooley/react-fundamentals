fe4575f93753c45cb78ecea00e9adcbbf7fad735
Vince <vince.r.cooley@gmail.com>
2016-03-31 15:28:14 -0500

    Confirm battle container and component started

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react');
+
+function ConfirmBattle(props) {
+  return props.isLoading === true
+    ? <p> LOADING! </p>
+    : <p> CONFIRM BATTLE </p>
+}
+
+module.exports = ConfirmBattle;
---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+
+var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function() {
+    return {
+      isLoading: true,
+      playerInfo: []
+    }
+  },
+  componentDidMount: function() {
+    var query = this.props.location.query;
+    //fetch info from github and update state
+  },
+  render: function() {
+    return (
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        playersInfo={this.state.playersInfo} />
+    )
+  }
+});
+
+module.exports = ConfirmBattleContainer;
